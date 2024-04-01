c8775de90db7a6e09fd79e9174559b076a23684d
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-01 15:49:59 -0700

    index.js in progress 4/1/24

---
+++ 18 Advanced JS and DOM manipulation/Drum Kit Starting Files/index.js
@@ -0,0 +1,41 @@
+var buttonsArr = document.querySelectorAll('.drum');
+var tomOneSound = new Audio('./sounds/tom-1.mp3');
+for (let i = 0; i < buttonsArr.length; i += 1) {
+  buttonsArr[i].addEventListener('click', function () {
+    var whichButton = this.textContent;
+    switch (whichButton) {
+      case 'w':
+        var crashSound = new Audio('./sounds/crash.mp3');
+        crashSound.play();
+        break;
+      case 'a':
+        var kickSound = new Audio('./sounds/kick-bass.mp3');
+        kickSound.play();
+        break;
+      case 's':
+        var snareSound = new Audio('./sounds/snare.mp3');
+        snareSound.play();
+        break;
+      case 'd':
+        var tomOneSound = new Audio('./sounds/tom-1.mp3');
+        tomOneSound.play();
+        break;
+      case 'j':
+        var tomTwoSound = new Audio('./sounds/tom-2.mp3');
+        tomTwoSound.play();
+        break;
+      case 'k':
+        var tomThreeSound = new Audio('./sounds/tom-3.mp3');
+        tomThreeSound.play();
+        break;
+      case 'l':
+        var tomFourSound = new Audio('./sounds/tom-4.mp3');
+        tomFourSound.play();
+        break;
+
+      default:
+        console.log(this.textContent);
+        break;
+    }
+  });
+}
