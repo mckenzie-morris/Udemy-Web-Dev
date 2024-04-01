738e6072b9cf3745a392732ad3838269265c41b3
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-01 11:55:39 -0700

    dicee game finished 4/1/24

--- 17 The Dicee Game/index.js
+++ 17 The Dicee Game/index.js
@@ -1,1 +1,22 @@
 var randomNumber1 = Math.ceil(Math.random() * 6);
+var diceVar = 'dice' + randomNumber1;
+document
+  .querySelector('.img1')
+  .setAttribute('src', './images/' + diceVar + '.png');
+
+var randomNumber2 = Math.ceil(Math.random() * 6);
+var diceVarTwo = 'dice' + randomNumber2;
+document
+  .querySelector('.img2')
+  .setAttribute('src', './images/' + diceVarTwo + '.png');
+
+var headerVar;
+if (randomNumber1 === randomNumber2) {
+  headerVar = 'Draw!';
+} else if (randomNumber1 > randomNumber2) {
+  headerVar = '🚩 Player 1 Wins!';
+} else if (randomNumber1 < randomNumber2) {
+  headerVar = 'Player 2 Wins! 🚩';
+}
+
+document.querySelector('#header').textContent = headerVar;
