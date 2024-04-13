fc8373cef5711900fda98d64995ab4e64797b540
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-12 19:24:25 -0700

    Simon Game still in progress 4/12/24

--- 20 The Simon Game/game.js
+++ 20 The Simon Game/game.js
@@ -1,28 +1,65 @@
 const gamePattern = [];
 const userClickedPattern = [];
+let gameLevel = 1;
+let userClickIdx = 0;
+let clickEnabled = true;
 const buttonColors = ['red', 'blue', 'green', 'yellow'];
 
-const nextSequence = () => {
+// generate a random color, push the color to the game pattern, and increment the game level
+const nextColor = () => {
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColor = buttonColors[randomNumber];
-  return randomChosenColor;
+  buttonAnimation(randomChosenColor);
+  buttonSound(randomChosenColor);
+  gamePattern.push(randomChosenColor);
+  $('h1').text(`Level ${gameLevel}`);
+  console.log(gamePattern);
 };
 
+// initiate the game
 $(document).on('keypress', () => {
   if (!gamePattern.length) {
-    const output = nextSequence();
-    gamePattern.push(output);
-    buttonAnimation(output);
-    buttonSound(output);
+    nextColor();
   }
 });
 
+// event handler for button click
 $('.btn').on('click', (clickEvent) => {
-  const userChosenColor = clickEvent.target.id;
-  buttonClickedAnimation(userChosenColor);
-  buttonSound(userChosenColor);
-  userClickedPattern.push(userChosenColor);
+  if (gamePattern.length && clickEnabled === true) {
+    const userChosenColor = clickEvent.target.id;
+    buttonClickedAnimation(userChosenColor);
+    buttonSound(userChosenColor);
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    checkFunc(userClickIdx);
+  }
 });
+
+const gameOver = () => {
+  $('h1').text('Game Over, Press Any Key to Restart');
+  gameLevel = 0;
+  gamePattern.length = 0;
+  userClickedPattern.length = 0;
+};
+
+// check user's pattern against generated pattern
+const checkFunc = (clickIdx) => {
+  if (userClickedPattern[clickIdx] !== gamePattern[clickIdx]) {
+    return gameOver();
+  }
+  userClickIdx += 1;
+  if (userClickedPattern.length === gamePattern.length) {
+    setTimeout(nextColor, 1000);
+    clickEnabled = false;
+    const switchClickToggle = () => {
+      clickEnabled = true;
+    };
+    setTimeout(switchClickToggle, 1100);
+    gameLevel += 1;
+    userClickIdx = 0;
+    userClickedPattern.length = 0;
+  }
+};
 
 // computer button animation function
 const buttonAnimation = (whichColor) => {
