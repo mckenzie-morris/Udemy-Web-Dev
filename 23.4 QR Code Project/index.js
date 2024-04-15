b18b318a18dd851afe9ad392b4288ef28946028c
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-15 15:05:49 -0700

    comments added to 23.4 4/15/24

--- 23.4 QR Code Project/index.js
+++ 23.4 QR Code Project/index.js
@@ -8,6 +8,7 @@ import inquirer from 'inquirer';
 import qr from 'qr-image';
 import { createWriteStream, writeFile } from 'fs';
 
+// questions array to craft multiple user prompts (only using one for url prompt)
 const questions = [
   {
     type: 'input',
@@ -15,6 +16,7 @@ const questions = [
     message: 'Please enter the URL you wish to convert to a QR code image:',
     validate(value) {
       const pass = value.match(
+        // regex to test for proper URL ('https://www.' is optional)
         /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/i
       );
       if (pass) {
@@ -26,10 +28,12 @@ const questions = [
 ];
 
 inquirer.prompt(questions).then((answers) => {
+  // generate a .txt file with the value exctracted from the answers object
   writeFile('myGeneratedURL.txt', answers.url, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
+  // generate a .png file with with the value exctracted from the answers object
   const qr_png = qr.image(answers.url, { type: 'png' });
   qr_png.pipe(createWriteStream('myGeneratedQRcode.png'));
 });
