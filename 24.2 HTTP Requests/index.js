c112113a1b0d805c48aa179ec50ed7102d62140b
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-16 11:47:20 -0700

    24.2 added 4/16/24

--- 24.2 HTTP Requests/index.js
+++ 24.2 HTTP Requests/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('GET request successful');
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on ${port}.`);
+  console.log(`Server listening on port ${3000}.`);
 });
