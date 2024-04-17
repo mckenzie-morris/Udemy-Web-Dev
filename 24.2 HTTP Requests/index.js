b5ce0106c9ba7632f41fb236143db3f9031a0f09
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-16 17:58:29 -0700

    24.2 finished 4/16/24

--- 24.2 HTTP Requests/index.js
+++ 24.2 HTTP Requests/index.js
@@ -3,7 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('GET request successful');
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('<h2>Call me @:</h2><p>1-800-call-now</p>');
+});
+
+app.get('/about', (req, res) => {
+  res.send(
+    '<h2>About me:</h2><p>Full-Stack Software Engineer and former Certified Personal Trainer</p>'
+  );
 });
 
 app.listen(port, () => {
