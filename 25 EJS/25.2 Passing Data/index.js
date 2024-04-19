d309a5f1a6e279349fbdd723e4b75d9dd38f17cf
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-04-19 10:38:13 -0700

    25.2 finished 4/19/24

--- 25 EJS/25.2 Passing Data/index.js
+++ 25 EJS/25.2 Passing Data/index.js
@@ -6,13 +6,29 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/submit', (req, res, next) => {
+  const regex = /^[a-zA-Z]+$/;
+  if (req.body) {
+    if (!regex.test(req.body.fName) || !regex.test(req.body.lName)) {
+      const data = { badInput: true };
+      res.render('index.ejs', data);
+      return;
+    }
+  }
+  next();
+});
+
 app.get('/', (req, res) => {
   const getData = { header: 'Input your name below 👇' };
   res.render('index.ejs', getData);
 });
 
 app.post('/submit', (req, res) => {
-  const postData = { header: 'something' };
+  const postData = {
+    nextheader: 'something',
+    firstName: req.body.fName,
+    lastName: req.body.lName,
+  };
   res.render('index.ejs', postData);
 });
 
