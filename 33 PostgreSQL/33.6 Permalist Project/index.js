9a4d6d1efab51872965352441afcee95887aed76
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-07-16 16:53:07 -0700

    33.6 in progress 7/16/24

--- 33 PostgreSQL/33.6 Permalist Project/index.js
+++ 33 PostgreSQL/33.6 Permalist Project/index.js
@@ -1,33 +1,78 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
+
+const db = new pg.Client({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
+  { id: 1, title: 'Buy milk' },
+  { id: 2, title: 'Finish homework' },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
+const getAllItems = async () => {
+  const query = {
+    text: 'SELECT * FROM items',
+  };
+  const result = await db.query(query);
+  return result.rows;
+};
+
+app.get('/', async (req, res) => {
+  const items = await getAllItems();
+  console.log(items);
+  res.render('index.ejs', {
+    listTitle: 'Today',
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+app.post('/add', async (req, res) => {
+  const query = {
+    text: 'INSERT INTO items (title) VALUES($1) RETURNING *',
+    values: [req.body.newItem],
+  };
+  const result = await db.query(query);
+  res.redirect('/');
 });
 
-app.post("/edit", (req, res) => {});
+app.post('/edit', async (req, res) => {
+  const query = {
+    text: 'UPDATE items SET title = $1 WHERE id = $2 RETURNING *',
+    values: [req.body.updatedItemTitle, req.body.updatedItemId],
+  };
+  const result = await db.query(query);
+  res.redirect('/');
+});
 
-app.post("/delete", (req, res) => {});
+app.post('/delete', async (req, res) => {
+  const query = {
+    text: 'DELETE FROM items WHERE id = $1 RETURNING *;',
+    values: [req.body.deleteItemId],
+  };
+  const result = await db.query(query);
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
