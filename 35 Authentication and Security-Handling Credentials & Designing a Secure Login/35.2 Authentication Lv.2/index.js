f60b96ec02ffefcbc7153d16900962cd93bd143c
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-18 13:50:15 -0700

    35.2 complete 10/18/24

--- 35 Authentication and Security-Handling Credentials & Designing a Secure Login/35.2 Authentication Lv.2/index.js
+++ 35 Authentication and Security-Handling Credentials & Designing a Secure Login/35.2 Authentication Lv.2/index.js
@@ -1,77 +1,94 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "123456",
-  port: 5432,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
 });
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
+app.get('/', (req, res) => {
+  res.render('home.ejs');
 });
 
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
 });
 
-app.get("/register", (req, res) => {
-  res.render("register.ejs");
+app.get('/register', (req, res) => {
+  res.render('register.ejs');
 });
 
-app.post("/register", async (req, res) => {
+app.post('/register', async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
   try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+    const checkResult = await db.query('SELECT * FROM users WHERE email = $1', [
       email,
     ]);
 
     if (checkResult.rows.length > 0) {
-      res.send("Email already exists. Try logging in.");
+      res.send('Email already exists. Try logging in.');
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
+      // password hashing
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log('Error hashing password: ', err);
+        } else {
+          const result = await db.query(
+            'INSERT INTO users (email, password) VALUES ($1, $2)',
+            [email, hash]
+          );
+          console.log(result);
+          res.render('secrets.ejs');
+        }
+      });
     }
   } catch (err) {
     console.log(err);
   }
 });
 
-app.post("/login", async (req, res) => {
+app.post('/login', async (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const loginPassword = req.body.password;
 
   try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+    const result = await db.query('SELECT * FROM users WHERE email = $1', [
       email,
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = user.password;
+      const storedHashedPassword = user.password;
 
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
+      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+        if (err) {
+          console.log('Error comparing passwords: ', err);
+        } else {
+          if (result) {
+            res.render('secrets.ejs');
+          } else {
+            res.send('Incorrect Password');
+          }
+        }
+      });
     } else {
-      res.send("User not found");
+      res.send('User not found');
     }
   } catch (err) {
     console.log(err);
