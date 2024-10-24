e3fd433a4cf7b91c9c43916c7e93cda86e96436d
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-24 14:32:48 -0700

    36.3 completed 10/24/24

--- 36 React/36.3 JavaScript Expressions in JSX/src/index.jsx
+++ 36 React/36.3 JavaScript Expressions in JSX/src/index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
-
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fName = 'Mckenzie';
+const lName = 'Morris';
+const luckyNumber = 13;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p>Your lucky number is {luckyNumber}</p>
+  </div>,
+  document.getElementById('root')
+);
