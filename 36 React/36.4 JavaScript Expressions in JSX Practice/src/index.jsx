136e04e6455aa2be34dde9fbf082949168f0333e
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-24 15:05:58 -0700

    36.4 completed 10/24/24

--- 36 React/36.4 JavaScript Expressions in JSX Practice/src/index.jsx
+++ 36 React/36.4 JavaScript Expressions in JSX Practice/src/index.jsx
@@ -12,3 +12,19 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fName = 'Mckenzie';
+const lName = 'Morris';
+const date = new Date();
+const currentYear = date.getFullYear();
+
+
+ReactDOM.render(
+  <div>
+    <h1>Created by {`${fName} ${lName}`}</h1>
+    <h2>Copyright: {currentYear}</h2>
+  </div>,
+  document.getElementById('root')
+);
