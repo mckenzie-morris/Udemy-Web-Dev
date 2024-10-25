8c9042ce905cd2d75fc358fc2aae207cad854d29
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-25 11:59:29 -0700

    36.8 finished 10/25/24

--- 36 React/36.8 React Components/src/index.jsx
+++ 36 React/36.8 React Components/src/index.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import List from './List';
+
+/* React convention to use PascalCase in constructing components
+
+function Heading() {
+  return <h1>My favorite foods</h1>;
+}
+
+*/
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <Heading></Heading>
+    {/* Best practice to turn custom components into self-closing tags (if nothing
+    is being rendered between tags in the file) */}
+    <List />
   </div>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you're running this locally in VS Code use the commands:
