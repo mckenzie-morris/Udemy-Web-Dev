a3ed20ff565d335329356f583c69972359197f2d
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-28 09:38:56 -0700

    36.13 completed 10/28/24

--- 36 React/36.13 React Props/src/index.jsx
+++ 36 React/36.13 React Props/src/index.jsx
@@ -1,35 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt='avatar_img' />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card
+      name='Beyonce'
+      img='https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg'
+      tel='+123 456 789'
+      email='b@beyonce.com'
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
-
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
+    <Card
+      name='Chuck'
+      img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGEI-KhPHFFfEPKMIuU2jbiWsj7Y4XhbRzWd1gGxc7xnzuqOeZZ2PomJ9VBTnVfgsFSq8&usqp=CAU'
+      tel='+123 456 789'
+      email='ChuckyN@Norris.com'
     />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
   </div>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you're running this locally in VS Code use the commands:
