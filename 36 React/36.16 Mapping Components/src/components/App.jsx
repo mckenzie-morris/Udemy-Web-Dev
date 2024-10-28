7ff20760a1413240a03c9d36fda372f58fe80466
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-28 16:25:06 -0700

    36.16 completed 10/28/24

--- 36 React/36.16 Mapping Components/src/components/App.jsx
+++ 36 React/36.16 Mapping Components/src/components/App.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import Card from "./Card";
-import contacts from "../contacts";
+import React from 'react';
+import Card from './Card';
+import contacts from '../contacts';
 
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
+      <h1 className='heading'>My Contacts</h1>
+      {contacts.map((cVal) => {
+        return (
+          <Card
+            /* In order to prevent the following warning (from React) in the browser:
 
-      <Card
+            'Warning: Each child in a list should have a unique "key" prop.'
+
+             attach the below 'key' property, and ensure it has a unique value. React
+             treats the prop specifically called 'key' as a special prop that will
+             not be passed down thru the virtual DOM to any of the component's children       
+          */
+            key={cVal.id}
+            /* the below prop ('id') will be passed down thru the virtual DOM to 
+            child components */
+            id={cVal.id}
+            name={cVal.name}
+            img={cVal.imgURL}
+            tel={cVal.phone}
+            email={cVal.email}
+          />
+        );
+      })}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +45,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
