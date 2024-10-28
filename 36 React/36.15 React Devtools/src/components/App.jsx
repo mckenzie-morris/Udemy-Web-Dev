2c8e4a20725ae40a6444e2c20e9d65435e653222
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-28 15:43:17 -0700

    36.15 completed 10/28/24

--- 36 React/36.15 React Devtools/src/components/App.jsx
+++ 36 React/36.15 React Devtools/src/components/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Card from "./Card";
-import contacts from "../contacts";
+import React from 'react';
+import Card from './Card';
+import contacts from '../contacts';
 
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
+      <h1 className='heading'>My Contacts</h1>
 
       <Card
         name={contacts[0].name}
