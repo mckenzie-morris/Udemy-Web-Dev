94a0f437ac3cb7d84b6ce7d27b025ea987d5c06e
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-29 09:53:47 -0700

    36.19 completed 10/29/24

--- 36 React/36.19 ES6 Arrow Functions/src/index.jsx
+++ 36 React/36.19 ES6 Arrow Functions/src/index.jsx
@@ -1,35 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// var numbers = [3, 56, 2, 48, 5];
+const numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+const mappedNumbers = numbers.map(x => x * 2);
+console.log(mappedNumbers)
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+const filteredNumbers = numbers.filter(num => num < 10);
+console.log(filteredNumbers)
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+var reducedNumbers = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(reducedNumbers)
 
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+const foundNumber = numbers.find(num => num > 10);
+console.log(foundNumber)
 
 ////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+const foundIndex = numbers.findIndex(num => num > 10)
+console.log(foundIndex)
 
 // If you're running this locally in VS Code use the commands:
 // npm install
