f2787793df960ed4dee8478f180d99fb1d0d3d83
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-30 13:22:13 -0700

    36.24 completed 10/30/24

--- 36 React/36.24 UseState Hook Practice/src/components/App.jsx
+++ 36 React/36.24 UseState Hook Practice/src/components/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString().split(' ')[0]);
+
+  console.log(time);
+
+  const updateTime = () => {
+    return setTime(new Date().toLocaleTimeString().split(' ')[0]);
+  };
+
+  setInterval(updateTime, 1000)
+
   return (
-    <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+    <div className='container'>
+      <h1>{time}</h1>
+      <button >Get Time</button>
     </div>
   );
+
+  
 }
 
 export default App;
