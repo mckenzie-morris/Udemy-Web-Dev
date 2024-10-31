0f5f3fa467299a25280176d0ca4a6602005033be
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-10-31 14:51:57 -0700

    36.26 completed 10/31/24

--- 36 React/36.26 Event Handling in React/src/components/App.jsx
+++ 36 React/36.26 Event Handling in React/src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [headingState, setHeading] = useState('Hello');
+
+  const clickHandler = () => {
+    setHeading('Submitted');
+  };
+
+  const [colorState, setColor] = useState('white');
+
+  const mouseIn = () => {
+    setColor('black');
+  };
+  const mouseOut = () => {
+    setColor('white');
+  };
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+    <div className='container'>
+      <h1>{headingState}</h1>
+      <input type='text' placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: `${colorState}` }}
+        onClick={clickHandler}
+        onMouseEnter={mouseIn}
+        onMouseOut={mouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
