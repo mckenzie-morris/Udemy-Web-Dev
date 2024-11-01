8141fa1f681762c5f4f0a13bcc390ffafe54badb
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-01 11:15:22 -0700

    36.27 completed 11/1/24

--- 36 React/36.27 React Forms/src/components/App.jsx
+++ 36 React/36.27 React Forms/src/components/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+    return;
+  };
+
+  const [submission, setSubmission] = useState('');
+
+  const handleSubmit = (event) => {
+    return setSubmission(name);
+  };
+
   return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+    <div className='container'>
+      <h1>Hello {submission}</h1>
+      {/* note: the <input /> and <button /> elements are NOT wrapped in a <form />-
+      doing so would cause a page refresh on the button click (the form element's
+      built-in 'onsubmit' attribute/method), which would extrude the JavaScript
+      expression from the HTML element immediately following the page refresh */}
+      <input
+        onChange={handleChange}
+        type='text'
+        placeholder="What's your name?"
+        /* note: best practice dictates adding the HTML value attribute and setting it
+        to the React state that corresponds to how that value is being tracked, in 
+        order to prevent any possible conflicts */
+        value={name}
+      />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
