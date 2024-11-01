f3447ce6c12b062cec6d1ec79efa397fc608734d
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-01 15:58:35 -0700

    36.28 completed 11/1/24

--- 36 React/36.28 Class Components vs Hooks/src/components/ClassComponent.jsx
+++ 36 React/36.28 Class Components vs Hooks/src/components/ClassComponent.jsx
@@ -6,10 +6,24 @@ class ClassComponent extends React.Component {
     this.state = {
       count: 0
     };
+    /* this.increase = this.increase.bind(this);
+
+    lefthand-side 'this': refers to the instance of ClassComponent that’s being 
+    created by the new keyword
+
+    righthand-side (first) 'this': refers to the original increase method defined 
+    in the class, which, before binding, is an unbound function. it doesn’t yet 
+    know which 'this' context it should use.
+
+    righthand-side (second) 'this': refers to the component instance, binding the 
+    method to the instance so that 'this' in 'increase' will always point to 
+    ClassComponent */
     this.increase = this.increase.bind(this);
   }
 
   increase() {
+    // context: 'this' = ClassComponent
+    console.log(this)
     this.setState({ count: this.state.count + 1 });
   }
 
@@ -17,6 +31,8 @@ class ClassComponent extends React.Component {
     return (
       <div>
         <h1>{this.state.count}</h1>
+        {/* React calls this.increase as a callback; this.increase refers 
+        to the increase() method in the component’s instance */}
         <button onClick={this.increase}>+</button>
       </div>
     );
