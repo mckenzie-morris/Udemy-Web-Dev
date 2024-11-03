205acbc903c04cd035bdb2a39729f45f583593c5
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-03 13:16:24 -0800

    36.30 completed 11/3/24

--- 36 React/36.30 Changing Complex State Practice/src/components/App.jsx
+++ 36 React/36.30 Changing Complex State Practice/src/components/App.jsx
@@ -1,22 +1,51 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
   const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
+    fName: '',
+    lName: '',
+    email: '',
   });
 
+  const updateContact = (event) => {
+    const { name, value } = event.target;
+
+    setContact((prevState) => {
+      if (name === 'fName') {
+        return { fName: value, lName: prevState.lName, email: prevState.email };
+      } else if (name === 'lName') {
+        return { fName: prevState.fName, lName: value, email: prevState.email };
+      } else {
+        return { fName: prevState.fName, lName: prevState.lName, email: value };
+      }
+    });
+  };
+
   return (
-    <div className="container">
+    <div className='container'>
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={updateContact}
+          value={contact.fName}
+          name='fName'
+          placeholder='First Name'
+        />
+        <input
+          onChange={updateContact}
+          value={contact.lName}
+          name='lName'
+          placeholder='Last Name'
+        />
+        <input
+          onChange={updateContact}
+          value={contact.email}
+          name='email'
+          placeholder='Email'
+        />
         <button>Submit</button>
       </form>
     </div>
