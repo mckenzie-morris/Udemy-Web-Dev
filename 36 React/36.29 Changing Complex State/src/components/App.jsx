80b16037efe7ff4a9078bf996e0bee84ed1a6241
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-03 11:47:18 -0800

    36.29 completed 11/3/24

--- 36 React/36.29 Changing Complex State/src/components/App.jsx
+++ 36 React/36.29 Changing Complex State/src/components/App.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: '',
+    lName: '',
+  });
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName((prevState) => {
+      if (inputName === 'fName') {
+        return { fName: newValue, lName: prevState.lName };
+      } else {
+        return { fName: prevState.fName, lName: newValue };
+      }
+    });
+  };
+
   return (
-    <div className="container">
-      <h1>Hello</h1>
+    <div className='container'>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleChange}
+          value={fullName.fName}
+          name='fName'
+          placeholder='First Name'
+        />
+        <input
+          onChange={handleChange}
+          value={fullName.lName}
+          name='lName'
+          placeholder='Last Name'
+        />
         <button>Submit</button>
       </form>
     </div>
