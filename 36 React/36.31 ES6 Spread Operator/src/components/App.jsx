7a04c22cf89eb4879e0256e27d41e3ada1c785d8
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-04 09:45:45 -0800

    36.31 completed 11/4/24

--- 36 React/36.31 ES6 Spread Operator/src/components/App.jsx
+++ 36 React/36.31 ES6 Spread Operator/src/components/App.jsx
@@ -1,40 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
   const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
+    fName: '',
+    lName: '',
+    email: '',
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value
-        };
-      }
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
     });
   }
 
   return (
-    <div className="container">
+    <div className='container'>
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
@@ -42,21 +27,21 @@ function App() {
       <form>
         <input
           onChange={handleChange}
-          name="fName"
+          name='fName'
           value={contact.fName}
-          placeholder="First Name"
+          placeholder='First Name'
         />
         <input
           onChange={handleChange}
-          name="lName"
+          name='lName'
           value={contact.lName}
-          placeholder="Last Name"
+          placeholder='Last Name'
         />
         <input
           onChange={handleChange}
-          name="email"
+          name='email'
           value={contact.email}
-          placeholder="Email"
+          placeholder='Email'
         />
         <button>Submit</button>
       </form>
