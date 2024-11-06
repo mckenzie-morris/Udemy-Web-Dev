69252ee4278d7839f7dab13cf5d471615ba15d36
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-11-05 17:47:16 -0800

    36.35 completed 11/5/24

--- 36 React/36.35 Keeper App Part 3 Starting/src/components/App.jsx
+++ 36 React/36.35 Keeper App Part 3 Starting/src/components/App.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import CreateArea from './CreateArea';
+import { useState } from 'react';
 
 function App() {
+  const [notesArr, setNotesArr] = useState([]);
+
+  const handleClick = (newNote) => {
+    setNotesArr((prevState) => {
+      return [...prevState, newNote];
+    });
+  };
+
+  const handleDelete = (id) => {
+    setNotesArr((prevState) => {
+      return prevState.filter((cVal, idx) => {
+        return idx !== id;
+      });
+    });
+  };
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea clickFunc={handleClick} />
+      {notesArr.map((elmt, idx) => {
+        return (
+          <Note
+            key={idx}
+            id={idx}
+            deleteFunc={handleDelete}
+            title={elmt.title}
+            content={elmt.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- 36 React/36.35 Keeper App Part 3 Starting/src/components/CreateArea.jsx
+++ 36 React/36.35 Keeper App Part 3 Starting/src/components/CreateArea.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({ title: '', content: '' });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setNote((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name='title'
+          onChange={handleChange}
+          value={note.title}
+          placeholder='Title'
+        />
+        <textarea
+          onChange={handleChange}
+          name='content'
+          value={note.content}
+          placeholder='Take a note...'
+          rows='3'
+        />
+        <button
+          /* when the below call to:
+        
+        setNote({ title: '', content: '' }) 
+        
+        is made, it schedules a state update, but this update doesn't immediately 
+        reflect within the current synchronous function call. instead, React batches 
+        and applies state updates after the current event loop tick finishes. hence why:
+
+        props.clickFunc(note);
+
+        can be called after the above function call, but still transmit the correct state.
+        
+        state updates are asynchronous, allowing functions to access the current 
+        state even after calling a state update function.*/
+          onClick={() => {
+            setNote({ title: '', content: '' });
+            props.clickFunc(note);
+          }}
+          type='button'
+        >
+          Add
+        </button>
       </form>
     </div>
   );
